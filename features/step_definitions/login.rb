ec2532312ffa0145d33960c72d2237dd10d10c0a
RickySandi <rickysandis@gmail.com>
2021-11-28 22:07:43 -0400

    deleting console print

--- features/step_definitions/login.rb
+++ features/step_definitions/login.rb
@@ -45,7 +45,6 @@ end
 
 Then('I should be logged in') do
     sleep 25
-    puts "You are logged in"  # Write code here that turns the phrase above into concrete actions
     name = 'RICARDO SANDI SUAREZ'
     find(:xpath, '/html/body/div/div/div[4]/div[1]/div/div/div/span', :text => name)
 end
