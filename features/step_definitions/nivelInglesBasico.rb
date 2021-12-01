bb52f24e54d3064f55aabae6a2dd4e671196ba69
RickySandi <rickysandis@gmail.com>
2021-12-01 10:21:26 -0400

    fixing xpaths

--- features/step_definitions/nivelInglesBasico.rb
+++ features/step_definitions/nivelInglesBasico.rb
@@ -5,8 +5,9 @@ Given('I push the {string} button on the left menu of the page') do |string|
     find(:xpath, xpath).click
 end
 
-When('I see the {string} table on companies page') do |string|
+When('I see the {string} table on companies page') do |string| 
     find(:xpath, '/html/body/div/div/div[4]/div[2]/div/div[2]/div[2]/table')
+    
 end
 
 And('I click the filter button with the text {string}') do |string|
@@ -37,13 +38,14 @@ end
 #         |   Intermedio  |
 #         |   Avanzado    |
 
-When('I click on the new option button {string}') do |string|
-    xpath ="/html/body/div/div/div[4]/div[2]/div/div[1]/a[2]/a/p"
+When('I click on the new option button {string}') do |string|   
+    xpath =" /html/body/div/div/div[4]/div[2]/div/div[1]/a[4]/a"
     find(:xpath, xpath).click
+   
 end
 
 Then('I should only see egresades with {string} level of English') do |string|
-    xpath ="/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div[2]/div"
+    xpath ="/html/body/div/div/div[4]/div[2]/div/div[2]/div[2]/table/tbody/tr[1]/td[7]"
     find(:xpath,  xpath,:text => "Basico")
 end
 
