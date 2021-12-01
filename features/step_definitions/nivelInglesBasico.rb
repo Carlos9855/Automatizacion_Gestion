7e8786975aafa539aef9c150dbc24c4906947349
Erick Luis <erick_suarlim@hotmail.com>
2021-12-01 09:19:44 -0400

    check

---
+++ features/step_definitions/nivelInglesBasico.rb
@@ -0,0 +1,39 @@
+#Scenario Outline: filter egresades by english level   
+
+Given('I push the {string} button on the left menu of the page') do |string|
+    xpath = "/html/body/div/div/div[4]/div[2]/div/div[1]/a[4]/a/i"
+    find(:xpath, xpath).click
+end
+
+When('I see the {string} table on companies page') do |string|
+    find(:xpath, '/html/body/div/div/div[4]/div[2]/div/div[2]/div[2]/table')
+end
+
+And('I click the filter button with the text {string}') do |string|
+    xpath = "/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div[1]"
+    find(:xpath, xpath).click
+end
+
+And('I press the button with label {string} on the new options view') do |string|
+    xpath ="/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]"
+    find(:xpath, xpath).click
+end
+
+When(/^I click on the new option button "([^"]*)" $/) do |nivelingles|
+    case nivelingles
+        when "Basico"
+            xpath ="/html/body/div/div/div[4]/div[2]/div/div[1]/a[2]/a/p"
+        when "Intermedio"
+            xpath ="/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div/div[2]/div[3]/div[2]/div[2]"
+        when "Avanzado"
+            xpath="/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div/div[2]/div[3]/div[2]/div[3]"
+    end
+    find(:xpath, xpath).click
+end
+
+Then('I should only see egresades with {string} level of English') do |string|
+    expect(find(:xpath, '/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div[2]/div')).to have_content(string)
+end
+
+
+  
