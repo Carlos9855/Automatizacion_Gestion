8ca7b21b67c87012fb7aaeaee55c97accb95fd42
Erick Luis <erick_suarlim@hotmail.com>
2021-12-01 10:07:54 -0400

    Check 2

--- features/nivelInglesBasico.feature
+++ features/nivelInglesBasico.feature
@@ -13,18 +13,14 @@ Background:
   And I push the "Verificar Acceso" button 
   And I see the message "Bienvenide"
 
-Scenario Outline: filter egresades by english level          
+Scenario: filter egresades by english level          
   Given I push the "Empresas" button on the left menu of the page
   When I see the "Egresades" table on companies page
   And I click the filter button with the text "Filtrar" 
   And I press the button with label "Nivel de Ingles" on the new options view
-  When I click on the new option button "<nivelingles>"
-  Then I should only see egresades with "<nivelingles>" level of English
+  When I click on the new option button "Basico"
+  Then I should only see egresades with "Basico" level of English
 
-Examples:
-        | nivelingles   |
-        |   Basico      |
-        |   Intermedio  |
-        |   Avanzado    |
+
 
   
--- features/step_definitions/nivelInglesBasico.rb
+++ features/step_definitions/nivelInglesBasico.rb
@@ -1,7 +1,7 @@
-#Scenario Outline: filter egresades by english level   
+#Scenario: filter egresades by english level   
 
 Given('I push the {string} button on the left menu of the page') do |string|
-    xpath = "/html/body/div/div/div[4]/div[2]/div/div[1]/a[4]/a/i"
+    xpath = "/html/body/div/div/div[4]/div[2]/div/div[1]/a[4]"
     find(:xpath, xpath).click
 end
 
@@ -19,21 +19,35 @@ And('I press the button with label {string} on the new options view') do |string
     find(:xpath, xpath).click
 end
 
-When(/^I click on the new option button "([^"]*)" $/) do |nivelingles|
-    case nivelingles
-        when "Basico"
-            xpath ="/html/body/div/div/div[4]/div[2]/div/div[1]/a[2]/a/p"
-        when "Intermedio"
-            xpath ="/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div/div[2]/div[3]/div[2]/div[2]"
-        when "Avanzado"
-            xpath="/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div/div[2]/div[3]/div[2]/div[3]"
-    end
+# When(/^I click on the new option button "([^"]*)" $/) do |nivelingles|
+#     case nivelingles
+#         when "Basico"
+#             xpath ="/html/body/div/div/div[4]/div[2]/div/div[1]/a[2]/a/p"
+#         when "Intermedio"
+#             xpath ="/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div/div[2]/div[3]/div[2]/div[2]"
+#         when "Avanzado"
+#             xpath="/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div/div[2]/div[3]/div[2]/div[3]"
+#     end
+#     find(:xpath, xpath).click
+# end
+
+# Examples:
+#         | nivelingles   |
+#         |   Basico      |
+#         |   Intermedio  |
+#         |   Avanzado    |
+
+When('I click on the new option button {string}') do |string|
+    xpath ="/html/body/div/div/div[4]/div[2]/div/div[1]/a[2]/a/p"
     find(:xpath, xpath).click
 end
 
 Then('I should only see egresades with {string} level of English') do |string|
-    expect(find(:xpath, '/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div[2]/div')).to have_content(string)
+    xpath ="/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div[2]/div"
+    find(:xpath,  xpath,:text => "Basico")
 end
 
 
-  
+#/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div[2]/div
+
+#//*[@id="root"]/div/div[4]/div[2]/div/div[2]/div[1]/div[2]/div
