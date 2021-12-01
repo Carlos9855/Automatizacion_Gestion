62079d1ca8ce84af0e414e802b7b7d9a218d4c6e
Erick Luis <erick_suarlim@hotmail.com>
2021-12-01 19:05:29 -0400

    Terminada

--- features/step_definitions/filtrarEgresadesNodosEmpresa.rb
+++ features/step_definitions/filtrarEgresadesNodosEmpresa.rb
@@ -8,18 +8,18 @@ Given('I hit the empresas button on the left part of the main page') do
   end
   
   And('I hit on the filter button with the text {string}') do |string|
-    xpath = "/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div[1]"
+    xpath = "/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div"
     find(:xpath, xpath).click
   end
   
   And('I press the node button called {string} on the new options view') do |string|
-    xpath = "/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]"        
+    xpath = "/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div/div[2]/div[1]"             
     find(:xpath, xpath).click
   end
   
   When('I click on the new nodes options button {string}') do |string|
-    sleep= 10
-    xpath = "/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]"        
+    sleep 2
+    xpath = "/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/span"        
     find(:xpath, xpath).click
     # 
     # find("span", :text => string).click
