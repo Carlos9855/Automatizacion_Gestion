a7302b85531e21034aeadf0b10898e4330c3a192
RickySandi <rickysandis@gmail.com>
2021-12-02 16:54:36 -0400

    creating topico

--- features/step_definitions/alumnosView.rb
+++ features/step_definitions/alumnosView.rb
@@ -1,8 +1,4 @@
-When('I click the {string} tab') do |string|
-    if expect(page).to have_link('Alumnes', href: "/cursos/alumnes")
-        click_link('Alumnes')
-    end    
-end
+
 
 
 
--- features/step_definitions/see.rb
+++ features/step_definitions/see.rb
@@ -10,5 +10,8 @@ Then('I see the {string} list') do |string|
     elsif string.include? "/" and string.include? "-"
         xpath = '/html/body/div/div/div[4]/div[2]/div/div[2]/div/div[4]/span/div/div[1]'
         expect(find(:xpath, xpath)).to have_content(string)
+    elsif string == "Topicos"
+        find(:xpath, '/html/body/div/div/div[4]/div[2]/div/div[2]/div[1]/a[4]').text.should == "Topicos"
     end                 
 end
+
