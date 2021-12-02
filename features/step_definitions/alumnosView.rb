21790e85d4e70403422fecea11789639da57f8eb
RickySandi <rickysandis@gmail.com>
2021-12-01 22:31:08 -0400

    update

--- features/step_definitions/alumnosView.rb
+++ features/step_definitions/alumnosView.rb
@@ -3,20 +3,16 @@ Given('I press the {string} button on the left sidebar') do |string|
     xpath = "/html/body/div/div/div[4]/div[2]/div/div[1]/a[3]/a"
     find(:xpath, xpath).click
 end
+
 When('I click the {string} tab') do |string|
-    # xpath = 
-    # find(:xpath, "/html/body/div/div/div[4]/div[2]/div/div[2]/div/div[1]/a[2]").click
     if expect(page).to have_link('Alumnes', href: "/cursos/alumnes")
         click_link('Alumnes')
     end    
   end
-#   href="/cursos/alumnes"
-# /html/body/div/div/div[4]/div[2]/div/div[2]/div/div[1]/a[2]
   
   Then('I see the {string} list') do |string|
     find(:xpath, '/html/body/div/div/div[4]/div[2]/div/div[2]/div/div[2]/h2/div').text.should == "Lista Alumnes"
                  
   end
 
-# /html/body/div/div/div[4]/div[2]/div/div[2]/div/div[2]/h2/div
 
