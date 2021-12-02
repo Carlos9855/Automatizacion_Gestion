608ceb25e73c369efa666542c8d010844fb967e0
RickySandi <rickysandis@gmail.com>
2021-12-01 22:24:15 -0400

    working again

--- features/step_definitions/login.rb
+++ features/step_definitions/login.rb
@@ -13,7 +13,7 @@ And('I click the {string} button') do |string|
 end
 
 # And I click on the Iniciar con Google button
-And('I press the {string} button') do |string|
+And('I press the {string} button') do |string| 
     xpath = "/html/body/div/div/div[2]/form/div/div/div/div/div[2]/div[2]/span/div/div/div/div/div/div/div/div/div/div/div[1]/a/div[2]"
     find(:xpath, xpath).click
 end
